a9f91c756222338d03e49c25d29587b4643f8571
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-02-01 20:24:51 +0100

    Add settings for Cargo Drone inventory size

---
+++ cargo-drone/settings.lua
@@ -0,0 +1,11 @@
+data:extend{
+    {
+        type = "int-setting",
+        name = "cargo-drone-inventory-size",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 1,
+        maximum_value = 300,
+        order = "a",
+    },
+}
