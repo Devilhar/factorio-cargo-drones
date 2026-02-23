96a2381595cf2a89a2b63c83c43d1d1637b3b45f
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-02-23 18:43:38 +0100

    Moved scheduling logic from item_requests to scheduler

--- cargo-drone/scripts/constants.lua
+++ cargo-drone/scripts/constants.lua
@@ -10,12 +10,11 @@ constants.drone_has_burnt_result = prototypes.entity["cargo-drone"].burner_proto
 -- Cargo drones
 constants.drone_queue_distance = 20
 constants.random_tick_interval = 60
-constants.min_task_assign_interval = 60
 constants.heuristic_target_count_cost = 50
 
 -- Scheduling
+constants.min_schedule_interval = 60
 constants.max_actions = 10
-constants.max_scans_per_tick = 10
 constants.cooldown_ticks = 30
 
 -- Tickrates need to either be divisible by random_tick_interval, or random_tick_interval need to be divisible by the tickrate
--- cargo-drone/scripts/scheduler.lua
+++ cargo-drone/scripts/scheduler.lua
@@ -2,8 +2,30 @@
 local util      = require("util")
 
 local constants = require("scripts.constants")
+local ep        = require("scripts.entity_property")
+local rc        = require("scripts.requester_cooldown")
 local dt        = require("scripts.drone_tasks")
 local ir	    = require("scripts.item_requests")
+
+local states = {
+    reset_request_buffers           = 0,
+    collect_provider_moorings       = 1,
+    collect_requester_moorings      = 2,
+    collect_provider_items          = 3,
+    collect_requester_items         = 4,
+    collect_idle_drones             = 5,
+    sort_idle_drone                 = 6,
+    assign_task_to_drone_with_cargo = 7,
+    process_next_item_request       = 8,
+}
+
+local function try_create_and_get_surface_buffer(surface_index)
+    if not storage.scheduler.surface_buffer[surface_index] then
+        storage.scheduler.surface_buffer[surface_index] = ir.create_surface_buffer()
+    end
+
+    return storage.scheduler.surface_buffer[surface_index]
+end
 
 local function get_closest_drone_to_mooring(drones, mooring)
     local closest_index = nil
@@ -21,6 +43,74 @@ local function get_closest_drone_to_mooring(drones, mooring)
     end
 
     return closest_index
+end
+
+local function reset_request_buffers()
+    storage.scheduler.update_stage = 0
+
+    storage.scheduler.surface_buffer = {}
+    storage.scheduler.provider_buffer = {}
+    storage.scheduler.requester_buffer = {}
+
+    storage.scheduler.mooring_key = nil
+end
+
+local function collect_provider_moorings()
+    local providers = ep.get_cargo_drone_provider_moorings()
+
+    for provider_id, provider_data in pairs(providers) do
+        storage.scheduler.provider_buffer[provider_id] = provider_data.entity
+    end
+end
+local function collect_requester_moorings()
+    local requesters = ep.get_cargo_drone_requester_moorings()
+
+    for requester_id, requester_data in pairs(requesters) do
+        storage.scheduler.requester_buffer[requester_id] = requester_data.entity
+    end
+end
+
+local function collect_provider_items()
+    local provider = nil
+
+    storage.scheduler.mooring_key, provider = next(storage.scheduler.provider_buffer, storage.scheduler.mooring_key)
+
+    if storage.scheduler.mooring_key == nil then
+        return true
+    end
+
+    if not provider.valid then
+        return false
+    end
+
+    local surface_buffer = try_create_and_get_surface_buffer(provider.surface.index)
+
+    ir.add_items_to_provider(provider, surface_buffer.provider_items, surface_buffer.item_provider_lookup)
+
+    return false
+end
+local function collect_requester_items()
+    local requester = nil
+
+    storage.scheduler.mooring_key, requester = next(storage.scheduler.requester_buffer, storage.scheduler.mooring_key)
+
+    if storage.scheduler.mooring_key == nil then
+        return true
+    end
+
+    if not requester.valid then
+        return false
+    end
+
+    if rc.is_on_cooldown(storage.scheduler.mooring_key) then
+        return false
+    end
+
+    local surface_buffer = try_create_and_get_surface_buffer(requester.surface.index)
+
+    ir.add_items_to_requester(requester, surface_buffer.requester_items, surface_buffer.item_requester_lookup, surface_buffer.sorted_requesters)
+
+    return false
 end
 
 local function collect_idle_drones()
@@ -100,41 +190,65 @@ local function assign_task_to_drone_with_cargo()
         return false
     end
 
-    ir.assign_to_request_with_items(drone)
+    local surface_buffer = storage.scheduler.surface_buffer[storage.scheduler.key_surface]
+
+    if not surface_buffer then
+        return false
+    end
+
+    ir.assign_to_request_with_items(surface_buffer, drone)
     storage.scheduler.tickrate_buffer[drone.unit_number] = constants.drones_tickrates.every
 
     return false
 end
 
 local function process_next_item_request()
-    local drones = nil
-
-    if storage.scheduler.key_drone == nil then
-        storage.scheduler.key_surface, drones = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
+    if storage.scheduler.key_surface == nil then
+        storage.scheduler.key_surface = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
 
         if storage.scheduler.key_surface == nil then
             return true
         end
-    else
-        drones = storage.scheduler.idling_cargo_drones_empty[storage.scheduler.key_surface]
     end
 
-    local item_request = ir.get_next_item_request(storage.scheduler.key_surface)
+    local drones = storage.scheduler.idling_cargo_drones_empty[storage.scheduler.key_surface]
+
+    if next(drones) == nil then
+        storage.scheduler.key_surface = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
+
+        return storage.scheduler.key_surface == nil
+    end
+
+    local surface_buffer = storage.scheduler.surface_buffer[storage.scheduler.key_surface]
+
+    if not surface_buffer then
+        storage.scheduler.key_surface = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
+
+        return storage.scheduler.key_surface == nil
+    end
+
+    local item_request = ir.get_next_item_request(surface_buffer)
 
     if not item_request then
-        return false
+        storage.scheduler.key_surface = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
+
+        return storage.scheduler.key_surface == nil
     end
 
     local closest_index = get_closest_drone_to_mooring(drones, item_request.provider)
 
-    if closest_index ~= nil then
-        local drone = drones[closest_index]
+    if closest_index == nil then
+        storage.scheduler.key_surface = next(storage.scheduler.idling_cargo_drones_empty, storage.scheduler.key_surface)
 
-        table.remove(drones, closest_index)
-
-        ir.assign_item_request(drone, item_request)
-        storage.scheduler.tickrate_buffer[drone.unit_number] = constants.drones_tickrates.every
+        return storage.scheduler.key_surface == nil
     end
+
+    local drone = drones[closest_index]
+
+    table.remove(drones, closest_index)
+
+    ir.assign_item_request(surface_buffer, drone, item_request)
+    storage.scheduler.tickrate_buffer[drone.unit_number] = constants.drones_tickrates.every
 
     return false
 end
@@ -145,7 +259,7 @@ function scheduler.init()
     storage.scheduler = storage.scheduler or {}
 
     storage.scheduler.update_state = storage.scheduler.update_state or 0
-    storage.scheduler.last_assign_tick = storage.scheduler.last_assign_tick or 0
+    storage.scheduler.last_schedule_tick = storage.scheduler.last_schedule_tick or 0
 
     storage.scheduler.tickrate_buffer = storage.scheduler.tickrate_buffer or {}
 
@@ -155,70 +269,118 @@ function scheduler.init()
     storage.scheduler.idling_cargo_drones = storage.scheduler.idling_cargo_drones or {}
     storage.scheduler.idling_cargo_drones_empty = storage.scheduler.idling_cargo_drones_empty or {}
     storage.scheduler.idling_cargo_drones_with_cargo = storage.scheduler.idling_cargo_drones_with_cargo or {}
+
+    storage.scheduler.surface_buffer = storage.scheduler.surface_buffer or {}
+    storage.scheduler.provider_buffer = storage.scheduler.provider_buffer or {}
+    storage.scheduler.requester_buffer = storage.scheduler.requester_buffer or {}
+
+    storage.scheduler.mooring_key = storage.scheduler.mooring_key or nil
 end
 
 function scheduler.drone_destroyed(unit_number)
     storage.scheduler.tickrate_buffer[unit_number] = nil
 end
 
-function scheduler.tick(game_tick)
-    if storage.scheduler.update_state == 0 then
-        storage.scheduler.update_state = 1
-        ir.begin_update()
-    end
+local state_procs = {
+    [states.reset_request_buffers] = function()
+        reset_request_buffers()
 
-    local current_action = 0
+        return true
+    end,
+    [states.collect_provider_moorings] = function()
+        collect_provider_moorings()
 
-    if storage.scheduler.update_state == 2 then
-        for _ = 1, constants.max_actions - current_action do
+        return true
+    end,
+    [states.collect_requester_moorings] = function()
+        collect_requester_moorings()
+
+        return true
+    end,
+    [states.collect_provider_items] = function()
+        for _ = 1, constants.max_actions do
+            if collect_provider_items() then
+                storage.scheduler.mooring_key = nil
+
+                return true
+            end
+        end
+
+        return false
+    end,
+    [states.collect_requester_items] = function()
+        for _ = 1, constants.max_actions do
+            if collect_requester_items() then
+                storage.scheduler.mooring_key = nil
+
+                return true
+            end
+        end
+
+        return false
+    end,
+    [states.collect_idle_drones] = function()
+        collect_idle_drones()
+
+        return true
+    end,
+    [states.sort_idle_drone] = function()
+        for _ = 1, constants.max_actions do
             if sort_idle_drone() then
-                storage.scheduler.update_state = 3
                 storage.scheduler.key_surface = nil
                 storage.scheduler.key_drone = nil
 
-                break
+                return true
             end
-
-            current_action = current_action + 1
         end
-    end
 
-    if storage.scheduler.update_state == 3 and game_tick >= storage.scheduler.last_assign_tick + constants.min_task_assign_interval then
-        for _ = 1, constants.max_actions - current_action do
+        return false
+    end,
+    [states.assign_task_to_drone_with_cargo] = function()
+        for _ = 1, constants.max_actions do
             if assign_task_to_drone_with_cargo() then
-                storage.scheduler.update_state = 4
                 storage.scheduler.key_surface = nil
                 storage.scheduler.key_drone = nil
 
-                break
+                return true
             end
-
-            current_action = current_action + 1
         end
-    end
 
-    if storage.scheduler.update_state == 4 then
-        for _ = 1, constants.max_actions - current_action do
+        return false
+    end,
+    [states.process_next_item_request] = function()
+        for _ = 1, constants.max_actions do
             if process_next_item_request() then
-                storage.scheduler.last_assign_tick = game_tick
-                storage.scheduler.update_state = 0
                 storage.scheduler.key_surface = nil
                 storage.scheduler.key_drone = nil
 
-                break
+                return true
             end
-
-            current_action = current_action + 1
         end
+
+        return false
+    end,
+}
+
+function scheduler.tick(game_tick)
+    if game_tick < storage.scheduler.last_schedule_tick + constants.min_schedule_interval then
+        return
     end
 
-    if storage.scheduler.update_state == 1 then
-        if ir.run_update() then
-            collect_idle_drones()
+    local done = state_procs[storage.scheduler.update_state]()
 
-            storage.scheduler.update_state = 2
-        end
+    if not done then
+        return
     end
+
+    storage.scheduler.update_state = storage.scheduler.update_state + 1
+
+    if storage.scheduler.update_state <= states.process_next_item_request then
+        return
+    end
+
+    storage.scheduler.last_schedule_tick = game_tick
+    storage.scheduler.update_state = states.reset_request_buffers
 end
 
 return scheduler
