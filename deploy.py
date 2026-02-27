909aacb1ab8fbb5b07f1d0b6a900c1c5d6a3751e
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-02-27 22:12:52 +0100

    Added Github actions

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ import json
 import sys
 import shutil
 
-def deploy(temp_dir, deploy_to_mod_dir):
+def deploy(temp_dir, deploy_to_mod_dir, echo_file):
     with open('cargo-drone/info.json', 'r') as file:
         info_data = json.load(file)
 
@@ -17,12 +17,17 @@ def deploy(temp_dir, deploy_to_mod_dir):
     output_temp_path = temp_dir + 'cargo-drone_' + version
     output_filename = output_dir + 'cargo-drone_' + version
     
-    print('Copying files to ' + output_temp_path)
+    if not echo_file:
+        print('Copying files to ' + output_temp_path)
 
     shutil.copytree(src='cargo-drone', dst=output_temp_path, dirs_exist_ok=True)
 
-    print('Creating zip at ' + output_filename + '.zip')
+    if not echo_file:
+        print('Creating zip at ' + output_filename + '.zip')
 
     shutil.make_archive(output_filename, 'zip', temp_dir, 'cargo-drone_' + version)
 
-deploy(sys.argv[1], "--deploy_to_mod_dir" in sys.argv)
+    if echo_file:
+        print(output_filename + '.zip')
+
+deploy(sys.argv[1], "--deploy_to_mod_dir" in sys.argv, "--echo_file" in sys.argv)
