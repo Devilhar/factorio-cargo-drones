5540a7e793ef4ac8d629e88787fc90763c373d47
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-03-21 11:07:44 +0100

    Fixed docked_drone not being set during migration

--- cargo-drone/scripts/migration/15.lua
+++ cargo-drone/scripts/migration/15.lua
@@ -6,6 +6,19 @@ local function migrate_mooring(mooring)
 end
 
 return function()
+	-- The docked_drone was never set during previous migrations.
+	for unit_number, entity_data in pairs(storage.cargo_drones) do
+		local docked_mooring = storage.managed_entities[unit_number].properties["docked_mooring"]
+
+        if docked_mooring then
+            local mooring_data = storage.managed_entities[docked_mooring.unit_number]
+
+            if mooring_data then
+                mooring_data.properties["docked_drone"] = entity_data.entity
+            end
+        end
+	end
+
 	for _, entity_data in pairs(storage.cargo_drone_provider_mooring) do
 		migrate_mooring(entity_data.entity)
 	end
