e206b1be9c1ffdc037b9fc33be0033c424a74e6e
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-03-21 11:11:07 +0100

    Fixed Drone count being outputted by moorings with Drone count turned off after migration

--- cargo-drone/scripts/migration/15.lua
+++ cargo-drone/scripts/migration/15.lua
@@ -1,8 +1,44 @@
 
+local function get_filter_value(section, filter_name)
+    local filters = section.filters
+
+    for i, filter in ipairs(filters) do
+        if filter.value and filter.value.name == filter_name then
+            return filter.min
+        end
+    end
+
+    return nil
+end
+
 local function migrate_mooring(mooring)
     local cb = mooring.get_control_behavior()
 
     cb.add_section()
+
+	-- The drone count was not migrated correctly, causing it to output the count even when not enabled. So migrate again.
+    local section_settings = cb.get_section(1)
+    local section_drone_count = cb.get_section(4)
+
+    local drone_count_circuit = get_filter_value(section_settings, "signal-G")
+    local drone_signal_set = get_filter_value(section_settings, "signal-H")
+    local drone_filter = section_drone_count.filters[1]
+    local drone_count = 0
+
+    if drone_count_circuit then
+        drone_count = storage.managed_entities[mooring.unit_number].properties["drone_count"]
+    end
+
+    if drone_filter then
+        drone_filter.min = drone_count
+    elseif drone_signal_set == nil then
+        drone_filter = {
+            value = { type = "virtual", name = "signal-C", quality = "normal" },
+            min = drone_count
+        }
+    end
+
+    section_drone_count.filters = { drone_filter }
 end
 
 return function()
