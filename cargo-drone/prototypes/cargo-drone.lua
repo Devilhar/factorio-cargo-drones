854ceb763af95a892f6194b762f4cd82a73b7a05
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-07-20 12:40:28 +0200

    Fixed lightning exemption not working for modded planets

--- cargo-drone/prototypes/cargo-drone.lua
+++ cargo-drone/prototypes/cargo-drone.lua
@@ -219,14 +219,3 @@ data:extend({
 		results = { { type = "item", name = "cargo-drone", amount = 1 } }
 	}
 })
-
-if settings.startup["cargo-drone-lightning-exemption"].value then
-	for _, planet in pairs(data.raw["planet"]) do
-		if planet.lightning_properties then
-			table.insert(planet.lightning_properties.exemption_rules, {
-				type = "id",
-				string = "cargo-drone",
-			})
-		end
-	end
-end
