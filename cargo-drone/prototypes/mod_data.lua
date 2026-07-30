28064d929dd36902ae1ab901db4df66528dea6d0
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-07-30 18:03:47 +0200

    Continued deployer support for multiple drones types

--- cargo-drone/prototypes/mod_data.lua
+++ cargo-drone/prototypes/mod_data.lua
@@ -4,18 +4,8 @@ local mod_data = {
 	name = "cargo-drone-data",
 	data_type = "string",
 	data = {
-		drones = {
-            --[[
-			{
-				name = "cargo-drone",
-				-- Sprite used by the deployer to represent this drone
-				deployer_sprites = {
-					sprite = "__cargo-drone__/graphics/cargo-drone.png",
-					shadow = "__cargo-drone__/graphics/cargo-drone-shadow.png",
-				},
-			},
-            ]]
-		},
+		drones = {},
+		items = {}
 	},
 }
 
