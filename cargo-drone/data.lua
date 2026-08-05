bfd88f9f39da2e4916381b44c1aa730607a4e8b1
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2026-08-05 13:50:55 +0200

    Added all items into a separate item subgroup

--- cargo-drone/data.lua
+++ cargo-drone/data.lua
@@ -2,6 +2,7 @@
 require("prototypes.mod_data")
 
 require("prototypes.signal")
+require("prototypes.item_subgroup")
 
 require("prototypes.cargo-drone")
 require("prototypes.moorings")
---
+++ cargo-drone/prototypes/item_subgroup.lua
@@ -0,0 +1,9 @@
+
+data:extend{
+    {
+        type = "item-subgroup",
+        name = "cargo-drone",
+        group = "logistics",
+        order = "g[cargo-drone]",
+    },
+}
